5fe6e139563ac6e6e0ceaaeb16fc5433e5790216
Admin <Admin@INSTRUCTOR>
2019-04-25 08:30:47 +0100

    First Commit

--- 08_Types_GettingStarted/src/starter/Program.java
+++ 08_Types_GettingStarted/src/starter/Program.java
@@ -1,28 +1,35 @@
 package starter;
 
 class Program {
+	
+	private Account ac1, ac2;
+	private static int num1, num2;
+
+	
 	public static void main(String[] args) {
 		// Uncomment these calls as instructed in 
 		// the exercise guide
+		
+		Program p = new Program () ;
 
-//		testRefType();
-//		testValueType();
+		p.testRefType();
+		p.testValueType();
 //		
 //		
 //		
-//		testIntArray();
-//		testAccountArray();
+		p.testIntArray();
+		p.testAccountArray();
 
 	}
 
-	private static Account ac1, ac2;
+	
 
-	private static void displayBalances() {
+	private void displayBalances() {
 		System.out.printf("\n'ac1' has: %.2f 'ac2' has: %.2f\n",
 				ac1.getBalance(), ac2.getBalance());
 	}
 
-	private static void testRefType() {
+	private void testRefType() {
 		ac1 = new Account(100.0, "John Smith");
 		ac2 = ac1;
 		displayBalances(); // 1st time
@@ -35,7 +42,7 @@ class Program {
 
 	}
 
-	private static void passAccountByValue(Account ac) {
+	private  void passAccountByValue(Account ac) {
 		ac.deposit(100.0);
 	}
 
@@ -43,13 +50,12 @@ class Program {
 	
 	
 	
-	private static int num1, num2;
-
-	private static void displayInts() {
+	
+	private  void displayInts() {
 		System.out.printf("\n'num1' is %d    'num2' is %d\n", num1, num2);
 	}
 
-	private static void testValueType() {
+	private  void testValueType() {
 		num1 = 10;
 		num2 = num1;
 		displayInts(); // 1st time
@@ -62,31 +68,31 @@ class Program {
 
 	}
 
-	private static void passIntByValue(int i) {
+	private  void passIntByValue(int i) {
 		i++;
 	}
 
 	
 	// ***********
 	
-	private static void testIntArray() {
+	private  void testIntArray() {
 		int[] ages = { 13, 15, 17 };
 		processAges(ages);
 		System.out.println(ages[1]); // 15 or 25
 	}
 
-	private static void processAges(int[] ages) {
+	private  void processAges(int[] ages) {
 		ages[1] += 10;
 	}
 
-	private static void testAccountArray() {
+	private  void testAccountArray() {
 		Account[] accs = { new Account(100.00, "Fred"),
 				           new Account(200.00, "Jazz") };
 		processAccounts(accs);
 		System.out.println(accs[0].getHolder()); // Fred or Jazz?
 	}
 
-	private static void processAccounts(Account[] accs) {
+	private  void processAccounts(Account[] accs) {
 		Account temp = accs[0];
 		accs[0] = accs[1];
 		accs[1] = temp;
