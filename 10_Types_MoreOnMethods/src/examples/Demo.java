bf67d4b63c21fb787d50d54ac46c959cbea0b8fb
Admin <Admin@INSTRUCTOR>
2019-04-25 08:44:35 +0100

    Examples added and removed from demo

---
+++ 10_Types_MoreOnMethods/src/examples/Demo.java
@@ -0,0 +1,26 @@
+package examples;
+
+public class Demo {
+	
+	private boolean flag ;
+	private String name ;
+	private double balance ;
+	private int age ;
+	public double getBalance() {
+		return balance;
+	}
+	public int getAge() {
+		return age;
+	}
+	private boolean getFlag() {
+		return flag;
+	}
+	private void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+	
+	
+	
+	
+
+}
---
+++ 11_Types_Misc/src/examples/DayOfWeek.java
@@ -0,0 +1,35 @@
+package examples;
+
+public enum DayOfWeek {
+	
+	SUNDAY ( 1, "Sun"),
+	MONDAY ( 2, "Mon" ),
+	TUESDAY (3, "Tue"),
+	WEDNESDAY (4, "Wed"),
+	THURSDAY (5, "Thu"),
+	FRIDAY (6, "Fri"),
+	SATURDAY (7, "Sat") ;
+	
+	private int ordinal ;
+	private String abbr ;
+	
+	DayOfWeek ( int ord, String abbr ) {
+		this.ordinal = ord ;
+		this.abbr = abbr ;
+	}
+
+	public String getAbbr() {
+		return abbr;
+	}
+
+	public void setAbbr(String abbr) {
+		this.abbr = abbr;
+	}
+
+	public int getOrdinal() {
+		return ordinal;
+	}
+	
+	
+
+}
